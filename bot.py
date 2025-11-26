4f888da1ae45ae707c5a689581aae4803b310c8b
ziganorsk0-hue <ziganorsk0@gmail.com>
2025-11-26 12:01:39 +0500

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("8023374417:AAHqDHcwM5hzWBzeyJN0Rfe1TUaSc4IseEg")
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот для кухни 😊")
-
-bot.polling()
