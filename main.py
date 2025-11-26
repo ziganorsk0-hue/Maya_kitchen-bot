77c1dd005364d86691e54f87fbaa1610f0f58718
ziganorsk0-hue <ziganorsk0@gmail.com>
2025-11-26 23:47:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,22 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# === Handlers ===
+# --- Handlers ---
 @dp.message_handler(CommandStart())
 async def start(message: types.Message):
-    await message.answer("Бот работает на Render! 🎉")
+    await message.answer("Бот работает на Render ✔")
 
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(f"Ты отправил: {message.text}")
 
-# === Webhook settings ===
+# --- Webhook settings ---
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_URL = os.getenv("RENDER_EXTERNAL_URL") + WEBHOOK_PATH
 
-HOST = "0.0.0.0"
-PORT = int(os.getenv("PORT", 10000))
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.getenv("PORT", 10000))
+
 
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
@@ -33,13 +34,16 @@ async def on_startup(dp):
 async def on_shutdown(dp):
     await bot.delete_webhook()
 
-if __name__ == "__main__":
+
+def main():
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=HOST,
-        port=PORT
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
     )
+
+if __name__ == "__main__":
+    main()
